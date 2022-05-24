fbd92b27c06f03a11bedee7433885c678d54208f
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2022-05-24 13:23:02 +0800

    Popup Menu
    Added user interface, but interacting function isn't working

--- js/content.js
+++ js/content.js
@@ -31,7 +31,26 @@ function SampleWebPage()
     });
 }
 
-SampleWebPage();
+document.addEventListener("DOMContentLoaded", function(){
+
+    var sw1 = JSON.parse(localStorage.getItem("#sw1"))
+    if(!sw1){
+        document.querySelector("#sw1").click();
+    } //set the checkbox to false if the value stored is false
+
+    Clicked("#sw1");
+
+    if(document.querySelector("#sw1").checked){
+        SampleWebPage();
+        console.log("work")
+    }
+})  //document setup not working for now
+
+function Clicked(sw_id){
+    document.querySelector(sw_id).addEventListener("click",function(){
+        localStorage.setItem(sw_id, document.querySelector(sw_id).checked)
+    })
+}  //identify when switch is clicked
 
 
 
