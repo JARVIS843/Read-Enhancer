1eb1db692d618ab7eed8c18e0b0d561ba93e8ffc
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2022-05-24 23:43:21 +0800

    d
    d

--- js/content.js
+++ js/content.js
@@ -12,6 +12,22 @@ function ModifyText(textNodeContent)
     });
 }
 
+function ModifyTextUpgrade(textNodeContent) 
+{
+    return pronouncing.syllableCount(textNodeContent)
+    return textNodeContent.split(' ').map((word) => {
+        //TODO if the user wants numbers to be bolded    
+        //if(/\d/.test(word)) return word;
+
+        
+        var boldUp2 = Math.floor(Math.random() * Math.floor(word.length/2)); //TODO Add customizable length: 1/4 , 1/2 , 3/4 of a word etc..
+        return word.replace(word, `<b>${word.substring(0, boldUp2+1)}</b>${word.substring(boldUp2+1)}`);  //TODO Add customizable fonts & underline the words that are originally bolded
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log(ModifyTextUpgrade("abandon"))
+})
 
 function SampleWebPage()
 {
@@ -31,26 +47,26 @@ function SampleWebPage()
     });
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+// document.addEventListener("DOMContentLoaded", function(){
 
-    var sw1 = JSON.parse(localStorage.getItem("#sw1"))
-    if(!sw1){
-        document.querySelector("#sw1").click();
-    } //set the checkbox to false if the value stored is false
+//     var sw1 = JSON.parse(localStorage.getItem("#sw1"))
+//     if(!sw1){
+//         document.querySelector("#sw1").click();
+//     } //set the checkbox to false if the value stored is false
 
-    Clicked("#sw1");
+//     Clicked("#sw1");
 
-    if(document.querySelector("#sw1").checked){
-        SampleWebPage();
-        console.log("work")
-    }
-})  //document setup not working for now
+//     if(document.querySelector("#sw1").checked){
+//         SampleWebPage();
+//         console.log("work")
+//     }
+// })  //document setup not working for now
 
-function Clicked(sw_id){
-    document.querySelector(sw_id).addEventListener("click",function(){
-        localStorage.setItem(sw_id, document.querySelector(sw_id).checked)
-    })
-}  //identify when switch is clicked
+// function Clicked(sw_id){
+//     document.querySelector(sw_id).addEventListener("click",function(){
+//         localStorage.setItem(sw_id, document.querySelector(sw_id).checked)
+//     })
+// }  //identify when switch is clicked
 
 
 
